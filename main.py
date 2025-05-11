333edd0df0d38a8e4b88f921621d1266d28663b3
Ulugbek <qurbanovulugbek428@gmail.com>
2025-05-11 18:42:35 +0500

    anangi

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CHANNEL_USERNAME = '@buxgalterlik_xizmatlari'
 GROUP_ID = -1002407659338
 
 ADMIN_ID = 734139298  # o'zingizning Telegram ID raqamingizni yozing
-host = os.getenv("DB_HOST")
+host = 'postgresql://post:tiHjT2J1dBFDwohfIXFaR1f1I38smNU2@dpg-d0gafcjuibrs73fe5i9g-a/telegram_vg4s'
 
 logging.basicConfig(level=logging.INFO)
 
